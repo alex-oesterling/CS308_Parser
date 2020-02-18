b5362ac863354f1d0a55d63cf549ff72000a897d
Dana Mulligan <dana.mulligan@duke.edu>
2020-02-18 16:07:51 -0500

    added another package

---
+++ src/slogo/model/command/booleancommand/BooleanCommand.java
@@ -0,0 +1,5 @@
+package slogo.model.command.booleancommand;
+
+public class BooleanCommand {
+
+}
---
+++ src/slogo/model/command/mathcommand/MathCommand.java
@@ -0,0 +1,10 @@
+package slogo.model.command.mathcommand;
+
+import slogo.model.command.Command;
+
+public class MathCommand extends Command {
+
+  public MathCommand(){
+
+  }
+}
