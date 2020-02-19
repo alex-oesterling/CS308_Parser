a0041c3b63fc7d411f1d0091a6975fe2865718a6
Hannah Taubenfeld <hbt3@duke.edu>
2020-02-19 12:04:46 -0500

    very starter basics for scene

--- src/slogo/controller/Main.java
+++ src/slogo/controller/Main.java
@@ -1,5 +1,14 @@
 package slogo.controller;
 
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
@@ -7,7 +16,28 @@ public class Main {
     /**
      * Start of the program.
      */
+    public static final String TITLE = "Parser";
+    public static final int SIZE_WIDTH = 1000;
+    public static final int SIZE_HEIGHT = 800;
+    public static final Paint BACKGROUND = Color.AZURE;
+
+    private Group root;
+    private Stage stage;
+
+    public void start (Stage stage) {
+        Scene scene = setupScene();
+        this.stage = stage;
+        stage.setScene(scene);
+        stage.setTitle(TITLE);
+        stage.show();
+    }
+
+    private Scene setupScene(){
+        root = new Group();
+        return new Scene(root, SIZE_WIDTH, SIZE_HEIGHT, BACKGROUND);
+    }
+
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        launch(args);
     }
 }
