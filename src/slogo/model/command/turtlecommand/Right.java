e006da4781d9b31dd70e89474ce463fd89fadd6b
Tyler Meier <tkm22@duke.edu>
2020-02-22 17:37:53 -0500

    changed some things in comments and also added setheading class

--- src/slogo/model/command/turtlecommand/Right.java
+++ src/slogo/model/command/turtlecommand/Right.java
@@ -8,7 +8,7 @@ public class Right extends TurtleCommand {
      * Right constructor, to get the value of going right
      * Calls super and sets the right turn value
      * @param body the specific turtle being used, what the right value will be
-     * @param value the value. the value of how far it is turning right
+     * @param value the value of how far it is turning right
      */
     public Right(Turtle body, double value) {
         super(value);
