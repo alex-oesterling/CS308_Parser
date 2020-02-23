2a847a98252faa31e6bb75d4c046d156111d33be
Tyler Meier <tkm22@duke.edu>
2020-02-23 17:48:07 -0500

    clear screen

---
+++ src/slogo/model/command/turtlecommand/ClearScreen.java
@@ -0,0 +1,8 @@
+package slogo.model.command.turtlecommand;
+
+import slogo.model.Turtle;
+
+public class ClearScreen extends TurtleCommand {
+    public ClearScreen(Turtle body){
+    }
+}
--- src/slogo/model/command/turtlecommand/querycommand/QueryCommand.java
+++ src/slogo/model/command/turtlecommand/querycommand/QueryCommand.java
@@ -1,12 +1,12 @@
-package slogo.model.command.turtlecommand.querycommand;
-
-import slogo.model.Turtle;
-import slogo.model.command.turtlecommand.TurtleCommand;
-
-public class QueryCommand extends TurtleCommand {
-
-  public QueryCommand(Turtle body){
-    super(body);
-  }
-
-}
+//package slogo.model.command.turtlecommand.querycommand;
+//
+//import slogo.model.Turtle;
+//import slogo.model.command.turtlecommand.TurtleCommand;
+//
+//public class QueryCommand extends TurtleCommand {
+//
+//  public QueryCommand(Turtle body){
+//    super(body);
+//  }
+//
+//}
