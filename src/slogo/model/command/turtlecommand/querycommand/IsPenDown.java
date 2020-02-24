896a612601dd72a3ef171bd7c23ba1ab4862b808
Dana Mulligan <dana.mulligan@duke.edu>
2020-02-23 19:32:14 -0500

    fixed query commands to reflect how Command works

--- src/slogo/model/command/turtlecommand/querycommand/IsPenDown.java
+++ src/slogo/model/command/turtlecommand/querycommand/IsPenDown.java
@@ -4,8 +4,11 @@ import slogo.model.Turtle;
 
 public class IsPenDown extends QueryCommand {
 
+    /**
+     * Return 1 if the pen is down, 0 otherwise
+     * @param body turtle with the pen
+     */
     public IsPenDown(Turtle body){
         super(body.getDrawingStatus());
-
     }
 }
