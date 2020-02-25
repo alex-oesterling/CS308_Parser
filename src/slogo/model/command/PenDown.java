b8b02f0a333b65950ce6c1152a3d9579e09a5d62
Tyler Meier <tkm22@duke.edu>
2020-02-25 16:31:26 -0500

    did more work on the commands

--- src/slogo/model/command/PenDown.java
+++ src/slogo/model/command/PenDown.java
@@ -4,6 +4,7 @@ import slogo.model.Turtle;
 
 public class PenDown extends Command {
 
+    private Turtle t;
     private static final double DRAWING = 1;
     /**
      * Constructor for pen down which allows the turtle to  start drawing/use its pen to draw
@@ -11,7 +12,17 @@ public class PenDown extends Command {
      * @param body the current turtle that will have its pen set to down
      */
     public PenDown(Turtle body){
-        super(DRAWING);
-        body.setDrawing(DRAWING);
+        super();
+        t = body;
+    }
+
+    /**
+     * Allows the penDown command to be executed
+     * @return 1 as true, meaning the pen is down
+     */
+    @Override
+    public Double execute() {
+        t.setDrawing(DRAWING);
+        return DRAWING;
     }
 }
--- src/slogo/model/command/SetHeading.java
+++ src/slogo/model/command/SetHeading.java
@@ -4,6 +4,9 @@ import slogo.model.Turtle;
 
 public class SetHeading extends Command {
 
+    private Turtle t;
+    private Double heading;
+
     /**
      * Set Heading constructor, takes in the value and turns the turtle to that
      * specific value
@@ -11,6 +14,17 @@ public class SetHeading extends Command {
      * @param value the value/angle that the value will turn to
      */
     public SetHeading(Turtle body, Double value){
-        super(body.setHeadingAndGetDeltaTheta(value));
+        super();
+        t = body;
+        heading = value;
+    }
+
+    /**
+     * Allows the SetHeading command to be executed
+     * @return the value of the heading, where the turtle is facing
+     */
+    @Override
+    public Double execute() {
+        return t.setHeadingAndGetDeltaTheta(heading);
     }
 }
