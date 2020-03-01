da9a09f23d8a89eeb9a8538c49a976e21dc7ef07
Tyler Meier <tkm22@duke.edu>
2020-03-01 14:32:38 -0500

    user created commands working on

--- src/slogo/model/command/MakeVariable.java
+++
@@ -1,9 +0,0 @@
-package slogo.model.command;
-
-import java.util.List;
-
-public class MakeVariable extends Command {
-    public MakeVariable(String variable, List<String> commands){
-        super();
-    }
-}
