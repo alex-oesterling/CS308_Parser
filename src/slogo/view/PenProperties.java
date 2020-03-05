83e1867e80929433504513c8b41cba0d57b58afc
Hannah Taubenfeld <hbt3@duke.edu>
2020-03-05 14:37:09 -0500

    updated some layout things for pen properties window

--- src/slogo/view/PenProperties.java
+++ src/slogo/view/PenProperties.java
@@ -1,16 +1,18 @@
 package slogo.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
-import slogo.model.Turtle;
-
 import java.util.ResourceBundle;
 
 public class PenProperties {
@@ -22,10 +24,11 @@ public class PenProperties {
     public static final String DEFAULT_RESOURCE_FOLDER = RESOURCES + "/formats/";
     private static final String STYLESHEET = "styling.css";
 
-    public static final int SIZE_WIDTH = 500;
-    public static final int SIZE_HEIGHT = 400;
+    public static final int SIZE_WIDTH = 300;
+    public static final int SIZE_HEIGHT = 170;
     public static final int COLORPICKER_HEIGHT = 30;
-
+    public static final int HBOX_SPACING = 10;
+    public static final int VBOX_SPACING = 15;
 
     private Group root;
     private Scene myScene;
@@ -56,12 +59,24 @@ public class PenProperties {
 
     private VBox createGrid(){
         TextField textField = new TextField();
-        VBox vbox = new VBox(styler.createLabel(myResources.getString("PenColor")),
-                penColor(),
-                textField,
-                styler.createButton(myResources.getString("ChangePenCommand"), e->myVisualzer.getCurrentTurtle().changePenStatus()),
-                styler.createButton(myResources.getString("ChangePenWidthCommand"), e->myVisualzer.getCurrentTurtle().changePenWidth(Double.parseDouble(textField.getText()))));
-
+        textField.setPromptText(myResources.getString("TextfieldText"));
+        HBox hbox1 = new HBox(styler.createLabel(myResources.getString("PenColor")), penColor());
+        hbox1.setSpacing(HBOX_SPACING);
+        HBox hbox2 = new HBox(textField, styler.createButton(myResources.getString("ChangePenWidthCommand"), e->myVisualzer.getCurrentTurtle().changePenWidth(Double.parseDouble(textField.getText()))));
+        hbox2.setSpacing(HBOX_SPACING);
+        ComboBox comboBox = new ComboBox(FXCollections.observableArrayList(myResources.getString("PenUp"), myResources.getString("PenDown")));
+        comboBox.setOnAction(e->{
+            System.out.println(comboBox.getValue());
+            if(comboBox.getValue().equals("Put pen down")){
+                myVisualzer.getCurrentTurtle().changePenStatus(true);
+            } else{
+                myVisualzer.getCurrentTurtle().changePenStatus(false);
+            }
+        });
+        HBox hbox3 = new HBox(styler.createLabel(myResources.getString("ChangePenCommand")), comboBox);
+        hbox3.setSpacing(HBOX_SPACING);
+        VBox vbox = new VBox(hbox1, hbox2, hbox3);
+        vbox.setSpacing(VBOX_SPACING);
         return vbox;
     }
 
