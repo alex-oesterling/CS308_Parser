b0b5a998e4eeff8b187d93ec82d15457989448e2
Hannah Taubenfeld <hbt3@duke.edu>
2020-03-05 21:11:46 -0500

    refactored the shapepalette

--- src/slogo/view/ShapePalette.java
+++ src/slogo/view/ShapePalette.java
@@ -16,6 +16,12 @@ public class ShapePalette {
 
     public static final String TITLE = "Shape Palette";
     public static final Paint BACKGROUND = Color.AZURE;
+    public static final double[] SHAPE_INDICES = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0,
+                                                    15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0};
+    public static final String[] SHAPE_NAMES = {"turtle1.png", "turtle2.png", "turtle3.png", "turtle4.png", "turtle5.png",
+            "turtle6.png", "turtle7.png", "heart.png", "smile.png", "star.png", "basketball.png", "Coach_k.png", "Alex_OConnell.png", "Cassius_Stanley.png",
+            "Goldwire_Jordan.png", "Jack_White.png", "Javon_DeLaurier.png", "Joey_Baker.png", "Justin_Robinson.png", "Keenan_Worthington.png",
+            "Matt_hurt.png", "Michael_Savarino.png", "Mike_Buckmire.png", "Tre_Jones.png", "Vernon_Carey.png", "Wendell_Moore.png"};
     public static final int SIZE_WIDTH = 150;
     public static final int SIZE_HEIGHT = 500;
     public static final int VBOX_SPACING = 10;
@@ -45,31 +51,9 @@ public class ShapePalette {
         vbox = new VBox();
         vbox.setSpacing(VBOX_SPACING);
         map = new TreeMap<>();
-        map.put(0.0, "turtle1.png");
-        map.put(1.0, "turtle2.png");
-        map.put(2.0, "turtle3.png");
-        map.put(3.0, "turtle4.png");
-        map.put(4.0, "turtle5.png");
-        map.put(5.0, "turtle6.png");
-        map.put(6.0, "turtle7.png");
-        map.put(7.0, "heart.png");
-        map.put(8.0, "smile.png");
-        map.put(9.0, "star.png");
-        map.put(10.0, "Coach_k.png");
-        map.put(11.0, "Alex_OConnell.png");
-        map.put(12.0, "Cassius_Stanley.png");
-        map.put(13.0, "Goldwire_Jordan.png");
-        map.put(14.0, "Jack_White.png");
-        map.put(15.0, "Javon_DeLaurier.png");
-        map.put(16.0, "Joey_Baker.png");
-        map.put(17.0, "Justin_Robinson.png");
-        map.put(18.0, "Keenan_Worthington.png");
-        map.put(19.0, "Matt_hurt.png");
-        map.put(20.0, "Michael_Savarino.png");
-        map.put(21.0, "Mike_Buckmire.png");
-        map.put(22.0, "Tre_Jones.png");
-        map.put(23.0, "Vernon_Carey.png");
-        map.put(24.0, "Wendell_Moore.png");
+        for(int i=0; i<SHAPE_INDICES.length; i++){
+            map.put(SHAPE_INDICES[i], SHAPE_NAMES[i]);
+        }
         for (Double key : map.keySet()) {
             HBox hbox = new HBox();
             hbox.setSpacing(HBOX_SPACING);
