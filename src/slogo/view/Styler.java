7942df2256bbb83b361e2fb42df33d7ed8be8599
Alex Oesterling <axo@duke.edu>
2020-03-05 21:48:40 -0500

    refactoring styler and toolbar'

--- src/slogo/view/Styler.java
+++ src/slogo/view/Styler.java
@@ -1,8 +1,11 @@
 package slogo.view;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 public class Styler {
 
@@ -14,5 +17,40 @@ public class Styler {
         Button button = new Button(string);
         button.setOnAction(e);
         return button;
+    }
+
+    /**
+     * Handles the creation of MenuItem objects, applies a label to them as specified in the createLabel method and
+     * gives them an action on click.
+     * @param property - the label to be applied to the object specified in the .properties file
+     * @param handler - the action to occur when the menu item is clicked
+     * @return a MenuItem to be placed in a Menu object with a label and action on click
+     */
+    public MenuItem makeMenuItem(String property, EventHandler<ActionEvent> handler) {
+        MenuItem result = new MenuItem();
+        createLabel(property, result);
+        result.setOnAction(handler);
+        return result;
+    }
+
+    /**
+     * Handles the creation of Menu objects, applies a label to them as specified in the createLabel method
+     * @param property - the label to be applied to the menu, from the .properties file
+     * @return a Menu to be rendered on stage with a label
+     */
+    public Menu makeMenu(String property) {
+        Menu result = new Menu();
+        createLabel(property, result);
+        return result;
+    }
+
+    /**
+     * Handles the creation of Menu and MenuItem labels
+     * @param property - the label to be applied to the items, from the .properties file
+     * @param result - the menu item to which the label is applied
+     */
+    private void createLabel(String property, MenuItem result) {
+        String label = myResources.getString(property);
+        result.setText(label);
     }
 }
--- src/slogo/view/ToolBar.java
+++ src/slogo/view/ToolBar.java
@@ -28,12 +28,14 @@ public class ToolBar {
   private Stage myStage;
   private CommandLine myTerminal;
   private Visualizer myVisualizer;
+  private Styler myStyler;
 
   public ToolBar(Stage stage, Visualizer visualizer, ResourceBundle newResources){
     myStage = stage;
     myVisualizer = visualizer;
     myTerminal = visualizer.getTerminal();
     myResources = newResources;
+    myStyler = new Styler();
   }
 
   public Node setupToolBar(){
@@ -83,40 +85,9 @@ public class ToolBar {
     SlogoApp newApp = new SlogoApp(new Stage());
   }
 
-  /**
-   * Handles the creation of MenuItem objects, applies a label to them as specified in the createLabel method and
-   * gives them an action on click.
-   * @param property - the label to be applied to the object specified in the .properties file
-   * @param handler - the action to occur when the menu item is clicked
-   * @return a MenuItem to be placed in a Menu object with a label and action on click
-   */
-  private MenuItem makeMenuItem(String property, EventHandler<ActionEvent> handler) {
-    MenuItem result = new MenuItem();
-    createLabel(property, result);
-    result.setOnAction(handler);
-    return result;
-  }
 
-  /**
-   * Handles the creation of Menu objects, applies a label to them as specified in the createLabel method
-   * @param property - the label to be applied to the menu, from the .properties file
-   * @return a Menu to be rendered on stage with a label
-   */
-  private Menu makeMenu(String property) {
-    Menu result = new Menu();
-    createLabel(property, result);
-    return result;
-  }
 
-  /**
-   * Handles the creation of Menu and MenuItem labels
-   * @param property - the label to be applied to the items, from the .properties file
-   * @param result - the menu item to which the label is applied
-   */
-  private void createLabel(String property, MenuItem result) {
-    String label = myResources.getString(property);
-    result.setText(label);
-  }
+
 
   /**
    * Opens a file navigator dialogue and allows the user to select an .xml file for importing into
@@ -197,5 +168,9 @@ public class ToolBar {
     errorAlert.setHeaderText(message);
     errorAlert.setContentText("Please select another file");
     errorAlert.showAndWait();
+  }
+
+  private Node animationControls(){
+
   }
 }
